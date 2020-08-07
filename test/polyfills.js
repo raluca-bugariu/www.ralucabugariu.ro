bc4fa727971f550c827d8bb3be35838f1493b128
dan-stefan <dan.bugariu@gmail.com>
2020-08-07 03:01:01 +0300

    test angular

--- test/polyfills.js
+++ test/polyfills.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\danbu\Desktop\LabFiles\13 - Simple Routing\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\danbu\Desktop\LabFiles\11 - Simple ClientServer\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
